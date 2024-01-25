28cf229875c1579b85266aea118ac680bacdf74d
PSusmita <panjasusmita00@gmail.com>
2024-01-25 08:39:59 +0530

    Generate hash Password

--- user/services/user/auth/registration.js
+++ user/services/user/auth/registration.js
@@ -1,15 +1,24 @@
 const sequelize = require("sequelize");
+const bcrypt = require("bcrypt");
 const { User } = require("../../../models/index")
 const STATUS = require("../../../static/core/status/status");
 const ERROR = require("../../../static/core/error/errors");
+require("dotenv").config();
 
 module.exports = async (obj) => {
     try {
-        const registerUser = await User.create(obj);
-        if (registerUser)
-            return (STATUS?.TRUE);
-        else
+        if (obj && obj?.password) {
+            const hashPassword = bcrypt.hash(obj?.password, process.env.SALT);
+            Object.assign(obj, { "password": hashPassword });
+            const registerUser = await User.create(obj);
+            if (registerUser)
+                return (STATUS?.TRUE);
+            else
+                return (STATUS?.FALSE);
+        }
+        else {
             return (STATUS?.FALSE);
+        }
     } catch (error) {
         return await ERROR?.errorResponse(error);
     }

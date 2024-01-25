8780af65c6579a2faafb399a2b42f5459f2c39b7
PSusmita <panjasusmita00@gmail.com>
2024-01-25 09:59:16 +0530

    up

--- user/services/user/auth/checkPassword.js
+++ user/services/user/auth/checkPassword.js
@@ -1,5 +1,4 @@
 const bcrypt = require("bcrypt");
-const { User } = require("../../../models/index");
 const STATUS = require("../../../static/core/status/status");
 const ERROR = require("../../../static/core/error/errors");
 
